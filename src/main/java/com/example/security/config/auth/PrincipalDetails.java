49327c706b0518ee7e4239f090b8f6c260dbd0c9
Joonsung Gil <sa82trip@gmail.com>
2022-11-27 09:59:32 +0900

    feat(PrincipalDetails): OAuth2User로 implments하게 수정
    Security sessio에 UserDetails or OAuth2User두가지의 타입이 Authentication으로 담긴다. 이런다면 컨트롤러에서 로직을 타기 복잡해 지기 때문에, 우리가 만든 PrincipalDetails를 UserDetails만이 아닌 OAuth2User도 implements하게 만들자.

--- src/main/java/com/example/security/config/auth/PrincipalDetails.java
+++ src/main/java/com/example/security/config/auth/PrincipalDetails.java
@@ -3,9 +3,11 @@ package com.example.security.config.auth;
 import com.example.security.model.User;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 /*
 시큐리티가 /login 주소 요청이 오면 낚아채서 로그인을 진행
@@ -16,7 +18,7 @@ User 오브젝트타입 -> UserDetails 타입 객체
 * */
 // Security Session -> Authentication -> UserDetails(PrincipalDetails)
 @Data
-public class PrincipalDetails implements UserDetails {
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private User user;
 
@@ -66,5 +68,16 @@ public class PrincipalDetails implements UserDetails {
     public boolean isEnabled() {
         // 사이트에 1년 동안 로그인을 안하면 휴면으로 돌리는 설정이 필요할 때
         return true;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
+    }
+
+
+    @Override
+    public String getName() {
+        return null;
     }
 }
